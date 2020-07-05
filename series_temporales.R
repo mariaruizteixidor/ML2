0831eb396912ba522fad7c4232660328f43bd9de
mariaruizteixidor <m.ruizt.2019@alumnos.urjc.es>
2020-07-05 23:49:15 +0200

    arima adjusted and autoarima v1

--- series_temporales.R
+++ series_temporales.R
@@ -24,18 +24,27 @@ df_demanda <- df_ree %>%
 df_demanda$datetime <- as.POSIXct(df_demanda$datetime)
 df_demanda$date <- as.Date(df_demanda$datetime, format="%d/%m/%Y")
 
+# dividir TRAIN - TEST
+df_demanda_train = df_demanda[df_demanda$date < '2020-06-01',]
+df_demanda_test = df_demanda[df_demanda$date >= '2020-06-01',]
+
 # Transform into a tibble object
-df_demanda1 <- df_demanda %>%
+df_demanda_train <- df_demanda_train %>%
   as_tsibble(index = date) #key = c(energy), index = datetime
 
+df_demanda_test <- df_demanda_test %>%
+  as_tsibble(index = date) #key = c(energy), index = datetime# Descomposición clásica
+
+df_demanda <- df_demanda %>%
+  as_tsibble(index = date) #key = c(energy), index = datetime# Descomposición clásica
+
 # plot series temporales
-df_demanda1 %>%
+df_demanda_train %>%
   autoplot(energy) +
   labs(title = "", subtitle = "") 
 
-# Descomposición clásica
 #aditivo 
-df_demanda1 %>%
+df_demanda_train %>%
   model(classical_decomposition(energy, type = "additive")) %>%
   components() %>%
   autoplot() + xlab(" ") +
@@ -44,7 +53,7 @@ df_demanda1 %>%
 demanda_decomposed = classical_decomposition(energy, type = "additive")
 
 #multiplicativo
-df_demanda1 %>%
+df_demanda_train %>%
   model(classical_decomposition(energy, type = "multiplicative")) %>%
   components() %>%
   autoplot() + xlab(" ") +
@@ -53,23 +62,23 @@ df_demanda1 %>%
 # Diferenciación
 # Se observa que tras la diferenciación la serie pierde la tendencia, siendo claramente estacionaria en media.
 # yt = xt - x(t-1)
-df_demanda1 %>% autoplot( difference(energy)) +
+df_demanda_train %>% autoplot( difference(energy)) +
   xlab("  ") + ylab(" ") +
   ggtitle("  ")
 
 # Una forma de determinar más objetivamente si es necesario diferenciar es utilizar una prueba de raíz unitaria. 
 # En este contraste, la hipótesis nula es que los datos son estacionarios, y buscamos pruebas de que la hipótesis nula es falsa. En consecuencia, pequeños p-valores (por ejemplo, menos de 0,05) sugieren que es necesario diferenciar.
-df_demanda1 %>%
+df_demanda_train %>%
   features(energy, unitroot_kpss)
 #El p-valor es menor que 0.05, lo que indica que la hipótesis nula es rechazada.
 
-df_demanda1 %>%
+df_demanda_train %>%
   mutate(diff_close = difference(energy)) %>%
   features(diff_close, unitroot_kpss)
 #Esta vez, el estadístico de la prueba es diminuto, el p-valor es mayor que 0.05, y está dentro del rango que esperaríamos para los datos estacionarios. 
 
 #Comprobar si existen autocorrelaciones significaticas:
-df_demanda1 %>%
+df_demanda_train %>%
   mutate(diff_close = difference(energy)) %>%
   features(diff_close, ljung_box, lag = 10)
 
@@ -78,48 +87,66 @@ df_demanda1 %>%
 # Correlograma
 # Gráfico en el que representamos los valores de la función de autocorrelación empírica r k contra los retardos k = 1, 2, ... , M donde típicamente M ≪ N .
 # Vemos que  tiene muchos componentes significativas para algún retardo.
-df_demanda1 %>% ACF(difference(energy, 7)) %>% autoplot()
+df_demanda_train %>% ACF(difference(energy, 7)) %>% autoplot()
 
 #Autocorrelación parcial
 # Con o sin diferenciación???
 
 # correlación que resulta después de eliminar el efecto de cualquier correlación en retardos más cortos
 # autocorrelación entre los instantes t y t + k condicionada a  los valores que toma la serie en los instantes t + 1, t + 2, ... , t + k − 1 .
-df_demanda1 %>% PACF(difference(energy, 7)) %>% autoplot()
+df_demanda_train %>% PACF(difference(energy, 7)) %>% autoplot()
 
-df_demanda1_diff <- difference(df_demanda1$energy, 7)
+df_demanda1_diff <- difference(df_demanda_train$energy, 7)
 
 # ARIMA
 #Ver qué parámetros añadir p, d, q 
 # componente no estacional: 2 (con diferenciación)
 # componente estacional: 7 (con diferenciación)
-model1 = arima (df_demanda1$energy,  order = c (2,7,0), method="CSS")
-model1
 
+# MODELO AJUSTADO MANUALMENTE ---------------------------------------------------------------------------------------------------------
+model1 = Arima(df_demanda_train$energy,  order = c (5,1,0)) #, method="CSS"
+model1
+summary(model1)
 coeftest(model1) 
+
+# Raíces complejas dentro del círculo
+# Se puede comprobar que las tres raices de la parte autoregresiva están dentro del círculo unidad, por tanto el proceso es estacionario. 
+gg_arma(model1)
+
+# Representar serie + predicción data test (modelo auto)
+#prediccion = forecast(model_auto, h = 50) #df_demanda_test$energy,
+prediction_model1<-predict(model1, n.ahead = 30)$pred
+plot(df_demanda_train$energy,col="grey",lwd=1.5,ylab="Demanda energética train + forecast", type = 'l', xlim=c(0,529))
+lines(prediction_model1, col="blue",lwd=1.5)
+time_series_test = ts(df_demanda_test$energy, start = 519, end = 549)
+lines(time_series_test, col="green",lwd=1.5)
 
 # Residuals
 checkresiduals(model1)
 
-# Forecast
-autoplot(forecast(model1))
+checkresiduals(prediction_model1)
 
-# 2 
-fit2 <- df_demanda1 %>%
-  model(arima = ARIMA(energy ~ pdq(6,0,0) + PDQ(0,0,0)))
+# MODELO AJUSTADO AUTOMÁTICAMENTE ---------------------------------------------------------------------------------------------------
+model_auto = auto.arima(df_demanda_train$energy)
+model_auto
+summary(model_auto)
 
-report(fit2)
+# Representar serie + predicción data test (modelo auto)
+#prediccion = forecast(model_auto, h = 50) #df_demanda_test$energy,
+prediction_auto<-predict(model_auto, n.ahead = 30)$pred
+plot(df_demanda_train$energy,col="grey",lwd=1.5,ylab="Demanda energética train + forecast", type = 'l', xlim=c(0,529))
+lines(prediction_auto, col="blue",lwd=1.5)
+time_series_test = ts(df_demanda_test$energy, start = 519, end = 549)
+lines(time_series_test, col="green",lwd=1.5)
 
-# Raíces complejas dentro del círculo
-# Se puede comprobar que las tres raices de la parte autoregresiva están dentro del círculo unidad, por tanto el proceso es estacionario. 
-gg_arma(fit2)
+# errores
+checkresiduals(prediction_auto)
 
-# SARIMA
-fit <- df_demanda1 %>%
+# SARIMA ---------------------------------------------------------------------------------------------------------------------------------
+fit <- df_demanda_train %>%
   model(arima = ARIMA(energy ~ pdq(0,1,1) + PDQ(0,1,1)))
 
-# ver errores
-
-
 #predicción
-fit %>% forecast(h=12) %>% autoplot(df_demanda1)
+fit %>% forecast(h=12) %>% autoplot(df_demanda_train)
+
+# ver errores
